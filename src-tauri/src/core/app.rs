edbf4603bc1a2ff540cf2510e4bdc4f5be38b6f9
progre <progre+pgp@prgrssv.net>
2022-04-04 00:28:03 +0900

    refactor: refactor

--- src-tauri/src/core/app.rs
+++ src-tauri/src/core/app.rs
@@ -33,18 +33,37 @@ use crate::{
     },
 };
 
-#[derive(Clone)]
+async fn listen_rtmp_if_need(app: &Arc<App>) -> bool {
+    let mut rtmp_server = app.rtmp_server.lock().await;
+    let weak = Arc::downgrade(app);
+    rtmp_server.set_delegate(weak);
+    app.listen_rtmp_if_need(&mut rtmp_server, app.settings.lock().await.deref())
+        .await
+}
+
+async fn run_ui(app: &Arc<App>, initial_rtmp: String) {
+    let initial_channel_name = app.settings.lock().await.general_settings.channel_name[0].clone();
+    let weak = Arc::downgrade(app);
+    app.ui
+        .lock()
+        .await
+        .run(initial_rtmp, initial_channel_name, weak)
+        .await
+        .await
+        .unwrap();
+}
+
+fn show_file_error_dialog(message: &str) {
+    let none: Option<&tauri::Window> = None;
+    dialog::blocking::message(none, "Fatal", message);
+}
+
 pub struct App {
     yp_configs: Vec<YPConfig>,
     settings: Arc<Mutex<Settings>>,
     ui: Arc<Mutex<Ui>>,
     rtmp_server: Arc<Mutex<RtmpServer>>,
     broadcasting: Arc<Mutex<Broadcasting>>,
-}
-
-fn show_file_error_dialog(message: &str) {
-    let none: Option<&tauri::Window> = None;
-    dialog::blocking::message(none, "Fatal", message);
 }
 
 impl App {
@@ -63,32 +82,13 @@ impl App {
     pub async fn run() {
         let zelf = Arc::new(Self::new().await);
 
-        let initial_rtmp = {
-            let mut rtmp_server = zelf.rtmp_server.lock().await;
-            let weak = Arc::downgrade(&zelf);
-            rtmp_server.set_delegate(weak);
-            let listening = zelf
-                .listen_rtmp_if_need(&mut rtmp_server, zelf.settings.lock().await.deref())
-                .await;
-            if listening {
-                "listening"
-            } else {
-                "idle"
-            }
+        let initial_rtmp = if listen_rtmp_if_need(&zelf).await {
+            "listening"
+        } else {
+            "idle"
         };
 
-        let weak = Arc::downgrade(&zelf);
-        let handle = zelf
-            .ui
-            .lock()
-            .await
-            .run(
-                initial_rtmp.to_owned(),
-                zelf.settings.lock().await.general_settings.channel_name[0].clone(),
-                weak,
-            )
-            .await;
-        handle.await.unwrap(); // long long awaiting
+        run_ui(&zelf, initial_rtmp.to_owned()).await; // long long awaiting
     }
 
     async fn show_check_again_terms_dialog_if_expired(&self) -> bool {
@@ -122,6 +122,19 @@ impl App {
         self.ui.lock().await.set_rtmp(status.to_owned());
         listening
     }
+
+    async fn update_channel(&self, settings: &Settings) {
+        let broadcasting = self.broadcasting.lock().await;
+        if broadcasting.is_broadcasting() {
+            let res = broadcasting.update(&self.yp_configs, settings).await;
+            if let Some(err) = res.err() {
+                self.ui.lock().await.notify_failure(&err);
+                return;
+            }
+        }
+
+        save_settings_and_show_dialog_if_error(settings, show_file_error_dialog).await;
+    }
 }
 
 unsafe impl Send for App {}
@@ -142,16 +155,7 @@ impl UiDelegate for App {
         self.listen_rtmp_if_need(self.rtmp_server.lock().await.deref_mut(), &settings)
             .await;
 
-        let broadcasting = self.broadcasting.lock().await;
-        if broadcasting.is_broadcasting() {
-            let res = broadcasting.update(&self.yp_configs, &settings).await;
-            if let Some(err) = res.err() {
-                self.ui.lock().await.notify_failure(&err);
-                return;
-            }
-        }
-
-        save_settings_and_show_dialog_if_error(&settings, show_file_error_dialog).await;
+        self.update_channel(&settings).await;
     }
 
     async fn on_change_yellow_pages_settings(&self, yellow_pages_settings: YellowPagesSettings) {
@@ -163,16 +167,7 @@ impl UiDelegate for App {
         self.listen_rtmp_if_need(self.rtmp_server.lock().await.deref_mut(), &settings)
             .await;
 
-        let broadcasting = self.broadcasting.lock().await;
-        if broadcasting.is_broadcasting() {
-            let res = broadcasting.update(&self.yp_configs, &settings).await;
-            if let Some(err) = res.err() {
-                self.ui.lock().await.notify_failure(&err);
-                return;
-            }
-        }
-
-        save_settings_and_show_dialog_if_error(&settings, show_file_error_dialog).await;
+        self.update_channel(&settings).await;
     }
 
     async fn on_change_channel_settings(&self, channel_settings: ChannelSettings) {
@@ -181,16 +176,7 @@ impl UiDelegate for App {
         let mut settings = self.settings.lock().await;
         settings.channel_settings = channel_settings;
 
-        let broadcasting = self.broadcasting.lock().await;
-        if broadcasting.is_broadcasting() {
-            let res = broadcasting.update(&self.yp_configs, &settings).await;
-            if let Some(err) = res.err() {
-                self.ui.lock().await.notify_failure(&err);
-                return;
-            }
-        }
-
-        save_settings_and_show_dialog_if_error(&settings, show_file_error_dialog).await;
+        self.update_channel(&settings).await;
     }
 }
 
@@ -204,23 +190,22 @@ impl RtmpListenerDelegate for App {
         let rtmp_conn_port = {
             let mut broadcasting = self.broadcasting.lock().await;
             let settings = self.settings.lock().await;
-            match broadcasting.broadcast(&self.yp_configs, &settings).await {
+            let rtmp_conn_port = match broadcasting.broadcast(&self.yp_configs, &settings).await {
                 Ok(ok) => ok,
                 Err(err) => {
                     self.ui.lock().await.notify_failure(&err);
                     return;
                 }
-            }
+            };
+            self.ui.lock().await.set_rtmp("streaming".to_owned());
+            rtmp_conn_port
         };
-
-        self.ui.lock().await.set_rtmp("streaming".to_owned());
 
         let outgoing = connect(&format!("localhost:{}", rtmp_conn_port)).await;
         pipe(incoming, outgoing).await; // long long awaiting
 
-        self.ui.lock().await.set_rtmp("listening".to_owned());
-
         {
+            self.ui.lock().await.set_rtmp("listening".to_owned());
             let mut broadcasting = self.broadcasting.lock().await;
             let settings = self.settings.lock().await;
             match broadcasting
